1268154f7cc8eac85a826e88afa2a95d5c7fc316
Martin H. Normark <m@martinnormark.com>
2016-10-11 00:06:36 +0200

    First code commit :punch:

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConfigOptionsTest.Options;
+using Microsoft.Extensions.Options;
+using NamingTools;
+
+namespace ConfigOptionsTest.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly INamingService _namingService;
+
+		public HomeController(INamingService namingService)
+		{
+			_namingService = namingService;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = $"{_namingService.GetAppName()} description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View();
+		}
+	}
+}
---
+++ Options/MyOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigOptionsTest.Options
+{
+	public class MyOptions
+	{
+		public NamingOptions NamingOptions { get; set; }
+	}
+
+	public class NamingOptions
+	{
+		public WebAppNamingOptions WebApp { get; set; }
+	}
+
+	public class WebAppNamingOptions
+	{
+		public string MyAppName { get; set; }
+	}
+}
